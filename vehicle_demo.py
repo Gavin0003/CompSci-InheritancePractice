89814b99aa261f8107f0513ffd47772d31cf264f
Gavin0003 <gavin.pettit.stu@cedarpark.org>
2026-05-07 09:18:17 -0700

    Final

--- vehicle_demo.py
+++ vehicle_demo.py
@@ -2,4 +2,19 @@ import vehicles
 
 auto_mobile = vehicles.automobile('make', 'model', 'mileage', 'price')
 
-car = vehicles.Car(auto_mobile)
+
+car = vehicles.Car('Tesla', 'Y', 2864, 999999, 4)
+truck = vehicles.Truck('Ford', 'F150', 2187, 9999999, 2)
+suv = vehicles.SUV('', '', 4871, 99999, 5)
+list = [car, truck, suv]
+
+print()
+for vehicle in list:
+    print(f'The following {vehicle.get_name()} is in inventory: ')
+    print(f'Make: {vehicle.get_make()}')
+    print(f'Model: {vehicle.get_model()}')
+    print(f'Mileage: {vehicle.get_mileage()}')
+    print(f'Price: {vehicle.get_price()}')
+    print(f'{vehicle.get_var()}: {vehicle.get_variable()}')
+    print()
+    
--- vehicles.py
+++ vehicles.py
@@ -36,23 +36,33 @@ class Car(automobile):
         self.__doors = doors
     def set_doors(self, num_door):
         self.__doors = num_door
-    def get_doors(self):
+    def get_variable(self):
         return self.__doors
-
-class truck(automobile):
+    def get_name(self):
+        return 'car'
+    def get_var(self):
+        return 'Number of doors'
+class Truck(automobile):
     def __init__(self, make, model, mileage, price, drive_type):
         super().__init__(make, model, mileage, price)
         self.__drive_type = drive_type 
     def set_drive_type(self, drive_type):
         self.__drive_type = drive_type
-    def get_drive_type(self):
+    def get_variable(self):
         return self.__drive_type
-    
-class truck(automobile):
+    def get_name(self):
+        return 'truck'
+    def get_var(self):
+        return 'Drive type'
+class SUV(automobile):
     def __init__(self, make, model, mileage, price, passenger_capacity):
         super().__init__(make, model, mileage, price)
         self.__passenger_capacity = passenger_capacity 
     def set_passenger_capacity(self, passenger_capacity):
         self.__passenger_capacity = passenger_capacity
-    def get_passenger_capacity(self):
+    def get_variable(self):
         return self.__passenger_capacity
+    def get_name(self):
+        return 'suv'
+    def get_var(self):
+        return 'Passenger capacity'
